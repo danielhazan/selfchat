73d259e4708aa8d8ddcb3a48f63ba0439465810f
danielhazan <danielhazan28@gmail.com>
2019-03-21 12:08:37 +0200

    first

--- app/src/main/java/com/example/selfchatex1/MainActivity.java
+++ app/src/main/java/com/example/selfchatex1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.selfchatex1;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.edit_text);
+        initializeChat(editText);
+    }
+    private void initializeChat(final EditText editText){
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!(editText.getText().toString().equals(""))){
+                    TextView textView = findViewById(R.id.textView);
+                    textView.append(editText.getText().toString());
+                    editText.setText("");
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 }
