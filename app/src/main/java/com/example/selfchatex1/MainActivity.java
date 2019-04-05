aa9d42ea8ed558b62652b273a5449f3a96255ca9
danielhazan <danielhazan28@gmail.com>
2019-04-05 16:56:52 +0300

    v1

--- app/src/main/java/com/example/selfchatex1/MainActivity.java
+++ app/src/main/java/com/example/selfchatex1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.selfchatex1;
 
+import android.os.Parcelable;
 import android.os.PersistableBundle;
 
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.support.v7.widget.RecyclerView;
+import android.support.design.widget.Snackbar;
+import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,18 +20,28 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private chatAdapter.chatAdapter1 adapter = new chatAdapter.chatAdapter1(new ArrayList<String>());
     private RecyclerView recyclerView;
+    private ArrayList<String> strings = new ArrayList<>();
+    private Parcelable recycleState;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> stringArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,
+                false);
         final EditText editText = findViewById(R.id.edit_text);
         recyclerView = (RecyclerView) findViewById(R.id.recycler) ;
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,
-                false));
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
         initializeChat(editText);
+        adapter.notifyDataSetChanged();
+
+
+
     }
     private void initializeChat(final EditText editText){
         Button button = findViewById(R.id.button);
@@ -42,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
 //                    textView.append("\n");
 //                    textView.append(editText.getText().toString());
                     editText.setText("");
+                    recyclerView.scrollToPosition(adapter.getItemCount()-1);
+
+                }
+                else{
+                    View mainView = findViewById(R.id.main_layout_id);
+                    String msg = "you can't send an empty message";
+                    int duration = Snackbar.LENGTH_SHORT;
+                    Snackbar.make(mainView,msg,duration).show();
 
                 }
             }
@@ -50,7 +71,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        recycleState =layoutManager.onSaveInstanceState();
+        outState.putParcelable("recyclestate",recycleState);
+        outState.putStringArrayList("recyclerContent",adapter.getStrings());
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle state){
+        super.onRestoreInstanceState(state);
+        if(state != null){
+            recycleState = state.getParcelable("recyclestate");
+            stringArrayList = state.getStringArrayList("recyclerContent");
+        }
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(stringArrayList!= null){
+            adapter.getStrings().addAll(stringArrayList);
+        }
+        if(recycleState != null){
+            layoutManager.onRestoreInstanceState(recycleState);
+        }
     }
 }
--- app/src/main/java/com/example/selfchatex1/chatAdapter.java
+++ app/src/main/java/com/example/selfchatex1/chatAdapter.java
@@ -1,12 +1,14 @@
 package com.example.selfchatex1;
 import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class chatAdapter {
@@ -25,12 +27,18 @@ public class chatAdapter {
 
     }
     public static class chatAdapter1 extends RecyclerView.Adapter<chatHolder>{
+//    public static class chatAdapter1 extends ListAdapter<chatHolder> {
         protected List<String> strings;
         public chatAdapter1(List<String> strings){
             this.strings = strings;
         }
+
         public void addItem(String item){
             this.strings.add(item);
+            notifyDataSetChanged();
+        }
+        public ArrayList<String> getStrings(){
+            return (ArrayList<String>) strings;
         }
 
 
